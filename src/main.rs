eb39c83a08ea633e8f19ba6e162e07ee4bdc78a6
mint1563 <yama.mana0707@gmail.com>
2025-04-29 19:51:49 +0900

    コメントの削除

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,15 @@ pub enum Message {
 
 impl Counter {
     pub fn view(&self) -> Column<Message> {
-        // We use a column: a simple vertical layout
         column![
-            // The increment button. We tell it to produce an
-            // `Increment` message when pressed
             button("+").on_press(Message::Increment),
-            // We show the value of the counter here
             text(self.value).size(50),
-            // The decrement button. We tell it to produce a
-            // `Decrement` message when pressed
             button("-").on_press(Message::Decrement),
         ]
     }
 }
 
 impl Counter {
-    // ...
-
     pub fn update(&mut self, message: Message) {
         match message {
             Message::Increment => {
